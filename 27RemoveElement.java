0830bda757af88484df8e68adaf40287044c9e3a
mehkey <87057470+mehkey@users.noreply.github.com>
2022-03-28 23:57:46 -0400

    Create 27RemoveElement.java

---
+++ 27RemoveElement.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int removeElement(int[] nums, int elem) {
+        int len = nums.length;
+        for (int i = 0 ; i< len; ++i){
+            while (nums[i]==elem && i< len) {
+                nums[i]=nums[--len];
+            }
+        }
+        return len;
+    }
+}
