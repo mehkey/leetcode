6aaa105026465ca481ec7159f5b659a396c50160
mehkey <87057470+mehkey@users.noreply.github.com>
2022-04-19 23:55:35 -0400

    Create 36ValidSudoku.py

---
+++ python/36ValidSudoku.py
@@ -0,0 +1,55 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        
+        
+        def validRow( board: List[List[str]],row:int)-> bool:
+            m = {}
+            for i in range(0,9):
+                
+                n = board[i][row] 
+
+                #if n == "3":
+                    #print (n)
+                if not n == "." and m.get(n):
+                    #print("HEEEERE")
+                    return False
+                m[n] = n
+            return True
+        
+        def validColumn( board: List[List[str]],column:int)-> bool:
+            m = {}
+            for i in range(0,9):
+                
+                n = board[column][i] 
+                if not n == "." and m.get(n):
+                    return False
+
+                m[n] = n
+            return True
+        
+        def validSquare( board: List[List[str]],x:int,y:int)-> bool:
+            m = {}
+            for i in range(0,3):
+                for j in range(0,3):
+                    n = board[3*x+i][3*j+y] 
+                    if not n == "." and m.get(n):
+                        return False
+                    m[n] = n
+            return True
+            
+        for i in range(0,9):
+            if not validColumn(board,i):
+                return False
+        
+        for i in range(0,9):
+            if not validRow(board,i):
+                return False
+        
+        
+        for i in range(0,3):
+            for j in range(0,3):
+                if not validSquare(board,i,j):
+                    return False
+
+        return True
