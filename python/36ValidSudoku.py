e534ac094d932341d413999d0ea2ba7b710d6bbf
mehkey <87057470+mehkey@users.noreply.github.com>
2022-04-20 09:30:00 -0400

    Update

--- python/36ValidSudoku.py
+++ python/36ValidSudoku.py
@@ -14,6 +14,7 @@ class Solution:
                 m.add(n)
             return True
         
+        
         def validColumn( board: List[List[str]],column:int)-> bool:
             m = set()
             for i in range(0,9):
