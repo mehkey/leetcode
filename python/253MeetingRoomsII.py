a15978b59bf6aa84eff0504841d200551eac64a2
mehkey <87057470+mehkey@users.noreply.github.com>
2022-04-30 22:37:18 -0400

    Update 253MeetingRoomsII.py

--- python/253MeetingRoomsII.py
+++ python/253MeetingRoomsII.py
@@ -1,13 +1,36 @@
-def min_meeting_rooms(self, intervals: List[Interval]) -> int:
-        startTime = invervals.sort(key=lambda x : x[0]).copy()
-        endTime = intervals.sort(key=lambda y: y[1]).copy()
+from typing import (
+    List,
+)
+from lintcode import (
+    Interval,
+)
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+    def min_meeting_rooms(self, intervals: List[Interval]) -> int:
+
+        intervals.sort(key=lambda x : x.start)
+        startTime = intervals.copy()
+        intervals.sort(key=lambda y : y.end)
+        endTime =  intervals.copy()
 
         s = 0
         e = 0
         count = 0
         maxC = 0
         while s < len(intervals) :
-            if startTime[s] <= endTime[e]:
+            if startTime[s].start < endTime[e].end:
                 count += 1
                 s += 1
             else:
@@ -15,4 +38,10 @@ def min_meeting_rooms(self, intervals: List[Interval]) -> int:
                 e += 1
             maxC = max(maxC, count)
         return maxC
+
+
         
+
+
+
+
