0676c4f7d7104fac2513cbd22eacfa8feddf1b74
mehkey <87057470+mehkey@users.noreply.github.com>
2022-06-19 22:42:51 -0400

    Update 127WordLadder.py
    Fix Word Ladder with * path instead of word to word graph

--- python/127WordLadder.py
+++ python/127WordLadder.py
@@ -1,6 +1,107 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         
+        
+        
+        """
+        
+        GRAPH
+        
+        words
+        Hashamp -> List
+        hit -> [ hot]
+        hot -> [dot, lot]
+        ...
+        
+        DFS - dijkstra
+        BFS - belman ford
+        A * 
+        Floyd Warshall 
+        
+        
+        
+        """
+        
+        
+        """graph = {}
+        
+        wordList.append(beginWord)
+        
+        def compare(word1,word2):
+            count = 0
+            #print(word1)
+            #print(word2)
+
+            for i in range(len(word1)):
+                if word1[i] != word2[i]:
+                    count += 1
+            #print(count)
+            return count == 1
+                
+        
+        for i in range(0,len(wordList)-1):
+            for j in range(i+1, len(wordList)):
+                word1 ,word2 = wordList[i], wordList[j]
+                if word1 != word2 and compare(word1,word2):
+                    #print("HERE")
+                    if word1 not in graph:
+                        graph[word1] = []
+                    graph[word1].append(word2)
+                    if word2 not in graph:
+                        graph[word2] = []
+                    graph[word2].append(word1)
+        
+
+        q = collections.deque()
+        #q[0]
+        #q[-1]
+        #q.pop()
+        #q.popLeft()
+        #q.append()
+        q.append(beginWord)
+        
+        level = 1
+        
+        visited = set()
+        visited.add(beginWord)
+        
+        #print(q)
+        graph["word"] = ["w1","w2"]
+        #print(graph)
+
+        while q :
+            
+            for i in range(len(q)):
+                
+                word = q.popleft()
+                #print(word)
+                if word == endWord:
+                    return level
+                
+                if word not in graph:
+                    continue
+                
+                for w in graph[word]:
+                    
+                    if not w in visited:
+                        q.append(w)
+                        visited.add(w)
+
+            level += 1
+            
+        return 0
+        
+        
+        
+        
+        
+        
+        
+        
+        """
+        
+        
+        """
         wordList.append(beginWord)
         
         graph = {}
@@ -56,5 +157,53 @@ class Solution:
         #O(n2)
         #O(n)
         
+        """
         
+        if not endWord in wordList:
+            return 0
+        
+        #graph = collections.defaultdict(list)
 
+        graph = {}
+        
+        wordList.append(beginWord)
+        
+        
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j+1:]
+                if pattern not in graph:
+                    graph[pattern] = []
+                graph[pattern].append(word)
+        
+        visited = set()
+        visited.add(beginWord)
+        
+        q = collections.deque()
+        #q[0]
+        #q[-1]
+        #q.pop()
+        #q.popLeft()
+        #q.append()
+        q.append(beginWord)
+        level = 1
+        while q :
+            
+            for i in range(len(q)):
+                
+                word = q.popleft()
+                #print(word)
+                if word == endWord:
+                    return level
+                
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j+1:]
+                    
+                    for w in graph[pattern]:
+                        if not w in visited:
+                            q.append(w)
+                            visited.add(w)
+
+            level += 1
+            
+        return 0
