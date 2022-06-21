5f740a0dffc1a586d4b46b627277ef1ae9f3767c
mehkey <87057470+mehkey@users.noreply.github.com>
2022-06-20 23:28:58 -0400

    Create 938BSTSumTotal.py

---
+++ python/938BSTSumTotal.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        
+        total = 0
+        def dfs(node):
+            nonlocal total
+            
+            if not node:
+                return
+            
+
+            if high >= node.val >= low:
+                #print(node.val)
+                total += node.val
+            
+            dfs(node.left)
+            dfs(node.right)
+            
+        
+        dfs(root)
+        return total
