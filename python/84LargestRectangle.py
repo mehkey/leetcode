5577a5783c5f72e421e0fef3c3f958749103cba4
mehkey <87057470+mehkey@users.noreply.github.com>
2022-06-21 16:29:42 -0400

    Create 84LargestRectangle.py

---
+++ python/84LargestRectangle.py
@@ -0,0 +1,22 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        
+        
+        stack = []
+        
+        maxArea = 0
+        
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index,height = stack.pop()
+                maxArea = max(maxArea, height * ( i - index))
+                start = index
+            
+            stack.append((start,h))
+        
+        for index, h in stack:
+            maxArea = max(maxArea, h * (len(heights) - index))
+            
+        
+        return maxArea
