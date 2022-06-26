b84d2a1cbedcb9f77d33ebbffa166872a373de4e
mehkey <87057470+mehkey@users.noreply.github.com>
2022-06-25 23:10:49 -0400

    Create 102LevelOrderTraversal.py

---
+++ python/102LevelOrderTraversal.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+        
+        if not root:
+            return []
+        
+        res = []
+        
+        q = deque()
+        
+        q.append(root)
+        
+        while q:
+            temp = []
+            for j in range(len(q)):
+                node = q.popleft()
+
+                temp.append(node.val)
+                
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            res.append(temp)
+
+        return res
