9e530fb68616945e1eac8085aed8838497a3f247
mehkey <87057470+mehkey@users.noreply.github.com>
2022-07-01 22:12:28 -0400

    Update 121BestTimeToBuyAndSellStocks.py

--- python/121BestTimeToBuyAndSellStocks.py
+++ python/121BestTimeToBuyAndSellStocks.py
@@ -15,3 +15,24 @@ class Solution:
             r+=1
         
         return maxProfit
+
+
+
+        class Solution:
+
+            
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        
+        minimum = float("inf")
+        
+        maximumdiff = float("-inf")
+        
+        for p in prices:
+            
+            if p - minimum > 0:
+                maximumdiff = max(maximumdiff, p - minimum)
+            
+            minimum = min(minimum, p)
+
+        return maximumdiff  if maximumdiff != float("-inf") else 0
