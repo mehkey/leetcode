b0ddc581da817f77375bd110e136a530078b26d2
mehkey <87057470+mehkey@users.noreply.github.com>
2022-07-05 23:05:09 -0400

    Create 895MaxFrequencyStack.py

---
+++ python/895MaxFrequencyStack.py
@@ -0,0 +1,23 @@
+class FreqStack:
+
+    def __init__(self):
+        self.heap = []
+        self.m = collections.defaultdict(int)
+        self.counter = 0
+        
+    def push(self, x):
+        self.m[x]+=1
+        heapq.heappush(self.heap,(-self.m[x], -self.counter, x))
+        self.counter+=1
+    
+    def pop(self):
+        toBeRemoved = heapq.heappop(self.heap)
+        self.m[toBeRemoved[2]]-=1
+        return toBeRemoved[2]
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
+
