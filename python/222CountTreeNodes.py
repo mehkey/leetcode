94944f531321bbdf69e14add23e09b240072721b
mehkey <87057470+mehkey@users.noreply.github.com>
2022-07-10 19:06:26 -0400

    Create 222CountTreeNodes.py

---
+++ python/222CountTreeNodes.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        
+        """
+        level ?
+        
+        lastlevel ?
+        
+        (sum(2*level-1)) + lastlevel
+        
+        
+        1      =1
+        1 2    =3
+        1 2 4  =4
+        1 2 4 8=15
+        ...
+
+
+        
+        
+        """
+        leftdepth = self.getdepth(root, True)
+        rightdepth = self.getdepth(root, False)
+
+        if leftdepth == rightdepth:
+            return 2 ** leftdepth - 1
+        else:
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+
+    def getdepth(self, root, isLeft):
+        if root is None:
+            return 0
+        if isLeft:
+            return 1 + self.getdepth(root.left, isLeft)
+        else:
+            return 1 + self.getdepth(root.right, isLeft)
