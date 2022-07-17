7b313ed90172c0809c3c726481eb26407171fff9
mehkey <87057470+mehkey@users.noreply.github.com>
2022-07-16 23:58:42 -0400

    Create 894AllPosibleBST.py

---
+++ python/894AllPosibleBST.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    @cache
+    def allPossibleFBT(self, N: int) -> List[Optional[TreeNode]]:
+        N -= 1
+        if N == 0: return [TreeNode(0)]
+        ret = []
+        for l in range(1, min(20, N), 2):
+            for left in self.allPossibleFBT(l):
+                for right in self.allPossibleFBT(N - l):
+                    root = TreeNode(0)
+                    root.left = left
+                    root.right = right
+                    ret += [root]
+        return ret
