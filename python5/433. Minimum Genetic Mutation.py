ed012320d9eb9131de78c2cab60899290b789152
mehkey <87057470+mehkey@users.noreply.github.com>
2022-11-09 12:21:47 -0500

    Create 433. Minimum Genetic Mutation.py

---
+++ python5/433. Minimum Genetic Mutation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        """
+        :type start: str
+        :type end: str
+        :type bank: List[str]
+        :rtype: int
+        """
+        queue = []
+        queue.append((start,0))
+        bankSet = set(bank)
+        
+        while queue:
+            curr, step = queue.pop(0)
+            if curr == end:
+                return step
+            for i in range(len(curr)):
+                for c in "AGCT":
+                    mutation = curr[:i] + c + curr[i+1:]
+                    if mutation in bankSet:
+                        bankSet.remove(mutation)
+                        queue.append((mutation,step+1))
+                        
+        return -1
