83b029a220346f384d020e97ccf94d3c8a2dad01
mehkey <87057470+mehkey@users.noreply.github.com>
2022-12-09 11:09:51 -0500

    Create 2136. Earliest Possible Day of Full Bloom.py

---
+++ python5/2136. Earliest Possible Day of Full Bloom.py
@@ -0,0 +1,14 @@
+class Solution:
+    def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
+        
+        cur_plant_time = 0
+        result = 0
+        indices = sorted(range(len(plantTime)), key=lambda x: -growTime[x])
+        
+        print(indices)
+        for i in indices:
+            cur_plant_time += plantTime[i]
+            print(cur_plant_time,  cur_plant_time + growTime[i])
+            result = max(result, cur_plant_time + growTime[i])
+        return result
+
