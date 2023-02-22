3d7f2d0e2c73af1381817f40ad45b8456b8faf67
mehkey <87057470+mehkey@users.noreply.github.com>
2023-02-21 23:12:36 -0500

    Update 155MinStackKeepASecondValueMinM.py

--- python/155MinStackKeepASecondValueMinM.py
+++ python/155MinStackKeepASecondValueMinM.py
@@ -11,7 +11,8 @@ class MinStack:
         :type x: int
         :rtype: nothing
         """
-        if not self.stack:self.stack.append((x,x)) 
+        if not self.stack:
+            self.stack.append((x,x)) 
         else:
             self.stack.append((x,min(x,self.stack[-1][1])))
 
@@ -19,21 +20,26 @@ class MinStack:
         """
         :rtype: nothing
         """
-        if self.stack: self.stack.pop()
+        if self.stack:
+            self.stack.pop()
 
     def top(self):
         """
         :rtype: int
         """
-        if self.stack: return self.stack[-1][0]
-        else: return None
+        if self.stack:
+            return self.stack[-1][0]
+        else:
+            return None
 
     def getMin(self):
         """
         :rtype: int
         """
-        if self.stack: return self.stack[-1][1]
-        else: return None
+        if self.stack: 
+            return self.stack[-1][1]
+        else: 
+            return None
 
 
 # Your MinStack object will be instantiated and called as such:
