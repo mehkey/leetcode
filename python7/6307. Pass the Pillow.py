cb7ba57fa536a57e977cb9cd92ec4b7bf1225054
mehkey <87057470+mehkey@users.noreply.github.com>
2023-03-08 22:22:51 -0500

    Update 6307. Pass the Pillow.py

--- python7/6307. Pass the Pillow.py
+++ python7/6307. Pass the Pillow.py
@@ -29,3 +29,13 @@ class Solution:
         
         return cur
             
+    
+    int rounds = time/(n-1);
+        int ans = 0;
+        if(rounds%2 == 0){
+            ans = (1+time%(n-1));
+        }else{
+            ans = (n-time%(n-1));
+        }
+        
+        return ans;
