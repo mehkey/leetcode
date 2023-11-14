9f9e0909f06c3f97fd3e8147de968ee24ca38243
mehkey <87057470+mehkey@users.noreply.github.com>
2023-11-13 23:42:59 -0500

    Create 2865. Beautiful Towers I.py

---
+++ python11/2865. Beautiful Towers I.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maximumSumOfHeights(self, maxHeights: List[int]) -> int:
+        
+        l = 0
+        mm = len(maxHeights)
+        
+        mmm = 0
+        
+        for start in range( mm ):
+
+            l = start -1
+            r = start +1
+            
+            tot = maxHeights[start]
+            cur = tot
+            while l >= 0 :
+                
+                cur = min(cur,maxHeights[l])
+                tot += cur
+                l-= 1
+            cur = tot
+            while r <= len(maxHeights) -1:
+                cur = min(cur,maxHeights[r])
+                tot += cur
+                r+= 1
+
+            mmm = max(mmm, tot)
+
+        return mmm
