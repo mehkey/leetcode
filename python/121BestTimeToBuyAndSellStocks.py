a5a34e227fca875a5a73d1731d278051206a21d1
mehkey <87057470+mehkey@users.noreply.github.com>
2023-12-17 23:52:00 -0500

    Update 121BestTimeToBuyAndSellStocks.py

--- python/121BestTimeToBuyAndSellStocks.py
+++ python/121BestTimeToBuyAndSellStocks.py
@@ -2,13 +2,15 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
         l = 0
+
         r = 1
+
         maxProfit = 0
-        
+
         while r < len(prices):
-            
+
             maxProfit = max(maxProfit, prices[r] - prices[l])
-            
+
             while l<r and prices[r] - prices[l] < 0:
                 l+=1
             
@@ -20,7 +22,7 @@ class Solution:
 
         class Solution:
 
-            
+
     def maxProfit(self, prices: List[int]) -> int:
         
         
