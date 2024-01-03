1c20224ff6e073c6f1804b8d72290ccce4568bc4
mehkey <87057470+mehkey@users.noreply.github.com>
2024-01-02 19:37:40 -0500

    Create 1291.SequentialDigits.py

---
+++ python13/1291.SequentialDigits.py
@@ -0,0 +1,28 @@
+lis = []
+
+for i in range(1,10):
+    cur = i
+    lis.append(cur)
+    for j in range(0,9):
+        prev = cur %10
+        if prev < 9:
+            cur *= 10
+            cur += prev + 1
+            lis.append(cur)
+
+lis.sort()
+
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        
+        #print(lis)
+        l = bisect_left(lis,low)
+        
+        r = bisect_right(lis,high)
+        
+        res = []
+        for val in range(l,r+1):
+            if val < len(lis) and low <= lis[val]<= high:
+                res.append(lis[val])
+                
+        return res
