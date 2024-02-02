74798cfe79bf567eced1cde6117c10bd84e4732a
mehkey <87057470+mehkey@users.noreply.github.com>
2024-02-01 23:17:51 -0500

    Create 1283. Find the Smallest Divisor Given a Threshold.py

---
+++ python13/1283. Find the Smallest Divisor Given a Threshold.py
@@ -0,0 +1,20 @@
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        N = len(nums)
+        l = 1
+        r = max(nums)
+        
+        t = inf
+        while l <= r:
+            mid = (l+r)//2
+            
+            res = sum([ ceil( n / mid ) for n in nums])
+            
+            if res <= threshold:
+                r = mid - 1
+                t = min(t, mid)
+            else:
+
+                l = mid + 1
+        
+        return t
