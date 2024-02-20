6d00c78f0c029996a93c1cabc70806a952ae2520
mehkey <87057470+mehkey@users.noreply.github.com>
2024-02-20 11:52:29 -0500

    Create 3007. Maximum Number That Sum of the Prices Is Less Than or Equal to K.py

---
+++ python18/3007. Maximum Number That Sum of the Prices Is Less Than or Equal to K.py
@@ -0,0 +1,16 @@
+
+
+class Solution:
+    def __init__(self):
+        self.x = None
+
+    def price(self, num):
+        res = 0
+        for i in range(self.x - 1, 64, self.x):
+            numModTwoTimesI = num % (1 << (i + 1))
+            res += ((num - numModTwoTimesI) >> 1) + max(numModTwoTimesI - (1 << i), 0)
+        return res
+    
+    def findMaximumNumber(self, k: int, x: int) -> int:
+        self.x = x
+        return bisect_right(range((1 << 63) - 1), k, key=self.price) - 2
