dd9c7e3cfab03188f57f8701eefb4865389b9ebe
mehkey <87057470+mehkey@users.noreply.github.com>
2024-04-21 23:59:26 -0400

    Create 1235. Maximum Profit in Job Scheduling.py

---
+++ python17/1235. Maximum Profit in Job Scheduling.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        N = len(profit)
+        arr = sorted(zip(startTime,endTime,profit))
+        arr2 = list(sorted(startTime))
+
+        @cache
+        def dp(i):
+            
+            if i >= N:
+                return 0
+            
+            res = dp(i+1)
+
+            cur = bisect.bisect_left(arr2, arr[i][1] )
+
+            res = max(res, dp(cur)  + arr[i][2])
+
+            return res
+
+        return dp(0)
