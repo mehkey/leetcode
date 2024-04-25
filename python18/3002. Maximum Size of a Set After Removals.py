75d1a1e70b23f82ae245ae757e9637188aee28b7
mehkey <87057470+mehkey@users.noreply.github.com>
2024-04-24 23:22:21 -0400

    Update 3002. Maximum Size of a Set After Removals.py

--- python18/3002. Maximum Size of a Set After Removals.py
+++ python18/3002. Maximum Size of a Set After Removals.py
@@ -15,34 +15,4 @@ class Solution:
 
         return min( min(len(u1), N//2) + min(len(u2), N//2) + len(shared), N )
 
-        '''
-        while i < N or j < N:
-            
-            if i != N and j != N:
-                if  nums1[i] <= nums2[j]:
-                    #print(i,j)
-                    if nums1[i] not in s:
-                        c += 1
-                        s.add(nums1[i])
-                    i+=1
-                elif nums2[j] <= nums1[i]:
-                    if nums2[j] not in s:
-                        c += 1
-                        s.add(nums2[j])
-                    j+=1
-            if i == N:
-                if nums2[j] not in s:
-                    c += 1
-                    s.add(nums2[j])
-                j+=1
-            if j == N:
-                if nums1[i] not in s:
-                    c += 1
-                    s.add(nums1[i])
-                i+=1
 
-            if c == N :
-                return len(s)
-        
-        return len(s)
-        '''
