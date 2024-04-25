26dc411d98a04907760d0e7022e521189ef1e73a
mehkey <87057470+mehkey@users.noreply.github.com>
2024-04-24 23:22:24 -0400

    Create 1233. Remove Sub-Folders from the Filesystem.py

---
+++ python17/1233. Remove Sub-Folders from the Filesystem.py
@@ -0,0 +1,35 @@
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+
+        '''
+        G = defaultdict(list)
+        
+        for f in folder:
+            
+            f = f.split('/')
+            
+            for i in range(1,len(f)-1):
+                G[f[i]].append(f[i+1])
+        
+        
+        def dfs():
+            
+        '''
+        s = set(folder)
+        
+        res = []
+        
+        for f in folder:
+            j = f
+            f = f.split('/')
+            cur = []
+            
+            found = False
+            for i in range(1,len(f)-1):
+                cur.append('/' + f[i])
+                if ''.join(cur) in s:
+                    found = True
+                    break
+            if not found:
+                res.append(j)
+        return res
