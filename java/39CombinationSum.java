6a6cbe78af72dcfa86336422fa56af13bcde9deb
mehkey <87057470+mehkey@users.noreply.github.com>
2024-06-20 23:55:01 -0400

    Update 39CombinationSum.java

--- java/39CombinationSum.java
+++ java/39CombinationSum.java
@@ -7,11 +7,13 @@ class Solution {
         ;
     }
 
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-        if(remain < 0) return;
-        else if(remain == 0) list.add(new ArrayList<>(tempList));
-        else{ 
-            for(int i = start; i < nums.length; i++){
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
+        if (remain < 0)
+            return;
+        else if (remain == 0)
+            list.add(new ArrayList<>(tempList));
+        else {
+            for (int i = start; i < nums.length; i++) {
                 tempList.add(nums[i]);
                 backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
                 tempList.remove(tempList.size() - 1);
@@ -22,5 +24,3 @@ class Solution {
         }
     }
 }
-
-
